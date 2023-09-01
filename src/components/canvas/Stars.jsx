aab0965744ccb456a19289bcc9e1401e2712939b
moonlightboss <cumrak.legenda@gmail.com>
2023-09-01 22:09:34 +0300

    Add moving stars with useFrame

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -7,6 +7,10 @@ const Stars = (props) => {
   const ref=useRef();
 
   const sphere = random.inSphere(new Float32Array(5000), {radius:1.2})
+  useFrame((state,delta)=>{
+    ref.current.rotation.x-=delta/10;
+    ref.current.rotation.y-=delta/15;
+  })
 
   return <>
   <group rotation={[0,0,Math.PI/ 4]}>
