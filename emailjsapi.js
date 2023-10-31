cfd3b4057ecdaf765b1cbc2dfbaaddc5e4ed8ed4
moonlightboss <cumrak.legenda@gmail.com>
2023-10-31 17:54:19 +0300

    gh pages

---
+++ emailjsapi.js
@@ -0,0 +1,8 @@
+const myKeys = {
+    serviceID: "service_tht127n",
+    templateID: "template_1eu74lu",
+    myEmail: "vladislavmoonlight111@gmail.com",
+    apiKey: "DBDelFs4_7Wj-LToC"
+};
+
+export { myKeys };
